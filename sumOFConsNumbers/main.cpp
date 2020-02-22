677b051ce1690e1fbd17701bf88880d554b1918d
Adeeb Mohammed <adeeb2358@gmail.com>
2020-02-22 15:19:26 +0900

    Initial Commit

---
+++ sumOFConsNumbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sumOFConsNumbers
+//
+//  Created by adeeb mohammed on 22/02/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
